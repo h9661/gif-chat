3f71790967a019bcbe74047731263a601bde4b93
h9661 <guscksdn123@gmail.com>
2023-09-15 23:02:16 +0900

    ws -> socket.io 변경

--- socket.js
+++ socket.js
@@ -1,31 +1,30 @@
-const WebSocket = require("ws");
+const SocketIO = require("socket.io");
 
 // similar with net module that i've used in boostcamp chanllange.
 module.exports = (server) => {
-  const wss = new WebSocket.Server({
-    server,
-    backlog: 1024,
-  });
+  // path: 클라이언트와 서버가 연결되는 경로를 지정해준다.
+  const io = SocketIO(server, { path: "/socket.io" });
 
-  wss.on("connection", (ws, req) => {
-    // get client ip address this method is popular. search and study.
+  io.on("connection", (socket) => {
+    const req = socket.request;
     const ip = req.headers["x-forwarded-for"] || req.connection.remoteAddress;
-    console.log("새로운 클라이언트 접속", ip);
-    ws.on("message", (message) => {
-      console.log(message);
+    console.log("새로운 클라이언트 접속!", ip, socket.id, req.ip);
+
+    socket.on("disconnect", () => {
+      console.log("클라이언트 접속 해제", ip, socket.id);
+      clearInterval(socket.interval);
     });
-    ws.on("error", (error) => {
+
+    socket.on("error", (error) => {
       console.error(error);
     });
-    ws.on("close", () => {
-      console.log("클라이언트 접속 해제", ip);
-      clearInterval(ws.interval);
+
+    socket.on("reply", (data) => {
+      console.log(data);
     });
-    const interval = setInterval(() => {
-      if (ws.readyState === ws.OPEN) {
-        ws.send("서버에서 클라이언트로 메시지를 보냅니다.");
-      }
+
+    socket.interval = setInterval(() => {
+      socket.emit("news", "Hello Socket.IO");
     }, 3000);
-    ws.interval = interval;
   });
 };
