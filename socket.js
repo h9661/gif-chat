d4dc4c21cefee191c1bb51cf36851a8a1aa5e0c6
h9661 <guscksdn123@gmail.com>
2023-09-21 21:20:26 +0900

    socket 설정 변경
    1. namespace 도입
    2. join 기능 활용으로 room 가입

--- socket.js
+++ socket.js
@@ -1,30 +1,28 @@
 const SocketIO = require("socket.io");
 
-// similar with net module that i've used in boostcamp chanllange.
-module.exports = (server) => {
-  // path: 클라이언트와 서버가 연결되는 경로를 지정해준다.
+module.exports = (server, app) => {
   const io = SocketIO(server, { path: "/socket.io" });
+  app.set("io", io);
+  const room = io.of("/room");
+  const chat = io.of("/chat");
 
-  io.on("connection", (socket) => {
-    const req = socket.request;
-    const ip = req.headers["x-forwarded-for"] || req.connection.remoteAddress;
-    console.log("새로운 클라이언트 접속!", ip, socket.id, req.ip);
+  room.on("connection", (socket) => {
+    console.log("room 네임스페이스에 접속");
+    socket.on("disconnect", () => {
+      console.log("room 네임스페이스 접속 해제");
+    });
+  });
+
+  chat.on("connection", (socket) => {
+    console.log("chat 네임스페이스에 접속");
+
+    socket.on("join", (data) => {
+      // data는 브라우저에서 보낸 방 아이디
+      socket.join(data); // join(id): 방 아이디에 들어가는 메서드
+    });
 
     socket.on("disconnect", () => {
-      console.log("클라이언트 접속 해제", ip, socket.id);
-      clearInterval(socket.interval);
+      console.log("chat 네임스페이스 접속 해제");
     });
-
-    socket.on("error", (error) => {
-      console.error(error);
-    });
-
-    socket.on("reply", (data) => {
-      console.log(data);
-    });
-
-    socket.interval = setInterval(() => {
-      socket.emit("news", "Hello Socket.IO");
-    }, 3000);
   });
 };
