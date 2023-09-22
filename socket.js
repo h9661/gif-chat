dcd62ce7e0e6ae74f889c85d9cbb6279f699aa77
h9661 <guscksdn123@gmail.com>
2023-09-22 22:54:41 +0900

    Socket.Io 객체에 session middleware 적용
    session middleware를 app과 socket 객체가 공유하게 해서 서로 같은 session 정보를 들고다닐 수 있게 된다.
    코드를 분석하는 데에 어렵지만, 계속봐서 익숙해져야 한다. 유용한 방법으로 보이기 때문이다.

--- socket.js
+++ socket.js
@@ -1,10 +1,17 @@
 const SocketIO = require("socket.io");
 
-module.exports = (server, app) => {
+module.exports = (server, app, sessionMiddleware) => {
   const io = SocketIO(server, { path: "/socket.io" });
   app.set("io", io);
   const room = io.of("/room");
   const chat = io.of("/chat");
+
+  // 모든 웹 소켓 연결 시마다 실행
+  // socket.request: 요청 객체에 접근 가능
+  // socket.request.res: 응답 객체에 접근 가능
+  chat.use((socket, next) => {
+    sessionMiddleware(socket.request, socket.request.res, next);
+  });
 
   room.on("connection", (socket) => {
     console.log("room 네임스페이스에 접속");
@@ -19,10 +26,38 @@ module.exports = (server, app) => {
     socket.on("join", (data) => {
       // data는 브라우저에서 보낸 방 아이디
       socket.join(data); // join(id): 방 아이디에 들어가는 메서드
+      // to(id).emit("join", ...): 특정 방에 데이터를 보내는 메서드
+      socket.to(data).emit("join", {
+        user: "system",
+        chat: `${socket.request.session.color}님이 입장하셨습니다.`,
+      });
     });
 
     socket.on("disconnect", () => {
       console.log("chat 네임스페이스 접속 해제");
+      const { referer } = socket.request.headers; // referer: 이전 페이지의 주소
+      const roomId = referer.split("/").at(-1); // 방 아이디 추출
+      socket.leave(roomId); // leave(id): 방 아이디에서 나가는 메서드
+      const currentRoom = socket.adapter.rooms[roomId];
+      const userCount = currentRoom ? currentRoom.length : 0;
+
+      if (userCount == 0) {
+        fetch(`http://localhost:3000/room/${roomId}`, {
+          method: "DELETE",
+        })
+          .then(() => {
+            room.emit("removeRoom", roomId);
+            console.log("방 제거 요청 성공");
+          })
+          .catch((error) => {
+            console.error(error);
+          });
+      } else {
+        socket.to(roomId).emit("exit", {
+          user: "system",
+          chat: `${socket.request.session.color}님이 퇴장하셨습니다.`,
+        });
+      }
     });
   });
 };
