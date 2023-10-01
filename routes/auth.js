dbbedd7c0012e9dc06f93a3cd46cee1225c0c85e
h9661 <guscksdn123@gmail.com>
2023-10-01 11:49:09 +0900

    [Feat] auth 몇가지 구현
    1. 로그인
    2. 로그아웃
    3. 회원가입

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,10 @@
+const passport = require("passport");
 const express = require("express");
 const router = express.Router();
+const ColorHash = require("color-hash").default;
+const bcrypt = require("bcryptjs");
 const { isLoggedIn, isNotLoggedIn } = require("../middlewares");
+const User = require("../schemas/user");
 
 router.get("/login", isNotLoggedIn, (req, res) => {
   res.render("login");
@@ -8,6 +12,53 @@ router.get("/login", isNotLoggedIn, (req, res) => {
 
 router.get("/signup", isNotLoggedIn, (req, res) => {
   res.render("signup");
+});
+
+router.post("/login", isNotLoggedIn, (req, res, next) => {
+  passport.authenticate("local", (err, user, info) => {
+    if (err) return next(err);
+    if (!user) return res.redirect(`/auth/login?error=${info.message}`);
+
+    req.logIn(user, (err) => {
+      if (err) return next(err);
+      return res.redirect("/");
+    });
+  })(req, res, next);
+});
+
+router.post("/signup", isNotLoggedIn, async (req, res, next) => {
+  let username = req.body.username;
+  let password = req.body.password;
+  let nick = req.body.nick;
+
+  const colorHash = new ColorHash();
+  let color = colorHash.hex(username);
+
+  console.log(username, color);
+
+  // hash password
+  const hash = await bcrypt.hash(password, 12);
+
+  const exUser = await User.findOne({ username: username });
+  if (exUser) {
+    return res.redirect("/auth/login?error=이미 가입된 회원입니다.");
+  } else {
+    const user = new User({
+      username: username,
+      password: hash,
+      nick: nick,
+      color: color,
+    });
+
+    await user.save();
+    return res.redirect("/");
+  }
+});
+
+router.get("/logout", isLoggedIn, (req, res) => {
+  req.logout();
+  req.session.destroy();
+  res.redirect("/");
 });
 
 module.exports = router;
